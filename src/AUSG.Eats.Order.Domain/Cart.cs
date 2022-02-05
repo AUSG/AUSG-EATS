c9702b0f76c5f0349203fbf8753bed8ffbd98573
Seongbin Kim <seongbin9786@gmail.com>
2022-02-05 11:39:46 +0900

    refactor: Removed redundant usage of 'this' and changed Id property to be nullable.

--- src/AUSG.Eats.Order.Domain/Cart.cs
+++ src/AUSG.Eats.Order.Domain/Cart.cs
@@ -2,43 +2,48 @@
 
 public class Cart
 {
-    public long UserId { get; set; }
+    public long? UserId { get; set; }
     
     // IList에는 AsReadOnly();가 없다. (why?)
     private readonly List<CartItem> _items = new List<CartItem>();
     
     // IReadOnlyCollection에서 hint로 IEnumerable로 변경함
     // hint로 Expression Body로 변경함
-    public IEnumerable<CartItem> Items => this._items.AsReadOnly();
+    public IEnumerable<CartItem> Items => _items.AsReadOnly();
 
+    public Cart(long? userId = null)
+    {
+        UserId = userId;
+    }
+    
     public void AddToCart(CartItem newItem)
     {
-        this._items.Add(newItem);
+        _items.Add(newItem);
     }
 
     public void RemoveFromCart(CartItem itemToRemove)
     {
-        this._items.Remove(itemToRemove);
+        _items.Remove(itemToRemove);
     }
 
     public void AlterCartItem(CartItem itemToChange)
     {
-        if (!this._items.Remove(itemToChange)) // Id 비교이므로 제거된다.
+        if (!_items.Remove(itemToChange)) // Id 비교이므로 제거된다.
             throw new ArgumentException("없는 CartItem을 변경하려고 한다.");
-        this._items.Add(itemToChange);
+        _items.Add(itemToChange);
     }
     
     public override bool Equals(object? obj)
     {
-        if ((obj == null) || !(this.GetType() == obj.GetType()))
+        if ((obj == null) || !(GetType() == obj.GetType()))
             return false;
         var another = (Cart) obj;
-        return this.UserId == another.UserId;
+        return UserId == another.UserId;
     }
 
     public override int GetHashCode()
     {
         // Non-readonly property referenced in 'GetHashCode()' ??
-        return this.UserId.GetHashCode();
+        return UserId.GetHashCode();
     }
 }
--- src/AUSG.Eats.Order.Domain/CartItem.cs
+++ src/AUSG.Eats.Order.Domain/CartItem.cs
@@ -2,27 +2,34 @@
 
 public class CartItem
 {
-    public long Id { get; set; }
+    public long? Id { get; set; }
     public List<CartItemOption> Options { get; set; }
     public int Quantity { get; set;  }
 
+    // C#에서 지원하는 신기한 생성자 상속(?) 문법
+    public CartItem(long? id, List<CartItemOption> options, int quantity) : this(options, quantity)
+    {
+        Id = id;
+    }
+
     public CartItem(List<CartItemOption> options, int quantity)
     {
-        this.Options = options;
-        this.Quantity = quantity;
+        Options = options;
+        Quantity = quantity;
     }
+    
     
     public override bool Equals(object? obj)
     {
-        if ((obj == null) || !(this.GetType() == obj.GetType()))
+        if ((obj == null) || !(GetType() == obj.GetType()))
             return false;
         var another = (CartItem) obj;
-        return this.Id == another.Id;
+        return Id == another.Id;
     }
 
     public override int GetHashCode()
     {
         // Non-readonly property referenced in 'GetHashCode()' ??
-        return this.Id.GetHashCode();
+        return Id.GetHashCode();
     }
 }
